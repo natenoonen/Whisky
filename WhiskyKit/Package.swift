d43e74c6a3a99a294a737ea117d1fbebda4bf2b1
Nate Noonen <natenoonen@gmail.com>
2025-04-04 18:24:22 -0500

    Update Package.swift

--- WhiskyKit/Package.swift
+++ WhiskyKit/Package.swift
@@ -31,21 +31,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "git@github.com:SwiftPackageIndex/SemanticVersion.git", from: "0.3.0"), 
-        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.8.1"), 
-        .package(url: "https://github.com/realm/SwiftLint.git", from: "0.54.0"), 
-        .package(url: "https://github.com/JohnSundell/CollectionConcurrencyKit.git",from:"0.2.0"), 
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git",from:"1.8.4"), 
-        .package(url: "https://github.com/jkandzi/Progress.swift",from:"0.4.0"), 
-        .package(url: "https://github.com/SwiftPackageIndex/SemanticVersion",from:"0.4.0"), 
-        .package(url: "https://github.com/jpsim/SourceKitten.git",from:"0.35.0"), 
-        .package(url: "https://github.com/sparkle-project/Sparkle",from:"2.7.0"), 
-        .package(url: "https://github.com/apple/swift-argument-parser.git",from:"1.5.0"), 
-        .package(url: "https://github.com/swiftlang/swift-syntax.git",from:"600.0.0"), 
-        .package(url: "https://github.com/realm/swiftlint",from:"0.58.2"), 
-        .package(url: "https://github.com/scottrhoyt/SwiftyTextTable",from:"0.9.0"), 
-        .package(url: "https://github.com/drmohundro/SWXMLHash.git",from:"7.0.2"), 
-        .package(url: "https://github.com/jpsim/Yams.git",from:"5.3.1")
+        .package(url: "git@github.com:SwiftPackageIndex/SemanticVersion.git", from: "0.3.0")
     ]
     targets: [
         .target(
